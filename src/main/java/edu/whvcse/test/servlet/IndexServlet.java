df281a671e6cfe45d23ac9cc46f66ba039e9b93a
liusong <17356393@qq.con>
2019-06-29 13:29:45 +0800

    1

--- src/main/java/edu/whvcse/test/servlet/IndexServlet.java
+++ src/main/java/edu/whvcse/test/servlet/IndexServlet.java
@@ -11,6 +11,7 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
+
 @WebServlet(urlPatterns ={"/index","/login"})
 public class IndexServlet extends HttpServlet {
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
